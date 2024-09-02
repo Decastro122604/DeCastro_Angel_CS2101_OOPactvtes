aaf07a3b02c0f856eaa407c5fc4c1a26f2ad60c4
Angel Lyka Mae De Castro <158176323+Decastro122604@users.noreply.github.com>
2024-09-02 10:01:03 +0800

    GettingInputs.java

---
+++ Laboratory Activity 1 - 02-09-24/GettingInputs.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class GettingInputs {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        
+        //Getting user inputs
+        System.out.print("Enter the year: ");
+        String year = scanner.nextLine();
+        
+        System.out.print("Enter the genre: ");
+        String genre = scanner.nextLine();
+        
+        System.out.print("Enter the album: ");
+        String album = scanner.nextLine();
+        
+        System.out.print("Enter the title: ");
+        String title = scanner.nextLine();
+        
+        System.out.print("Enter the artist: ");
+        String artist = scanner.nextLine();
+        
+        //Display the entered details
+        
+System.out.println("\n-------------------------------------");
+        System.out.print("SONG DETAILS");
+System.out.println("\n-------------------------------------");
+
+        System.out.println("Year Released: "+year);
+        System.out.println("Genre: "+genre);
+        System.out.println("Album: "+album);
+        System.out.println("Title: "+title);
+        System.out.println("Artist: "+artist);
+        
+        scanner.close();
+
+    }
+}
